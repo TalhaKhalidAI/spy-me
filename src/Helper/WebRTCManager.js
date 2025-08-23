a0351cfe16ebd610248e83b2b7d8017f11d5971f
iot-Noob <talhakhalid2018@gmail.com>
2025-08-23 19:48:29 +0500

    remove stun server

--- src/Helper/WebRTCManager.js
+++ src/Helper/WebRTCManager.js
@@ -3,19 +3,7 @@ export default class WebRTCManager {
     this._lastRemoteStream = null;
     this.config = config.iceServers
       ? config
-      : {
-          iceServers: [
-            { urls: "stun:stun.l.google.com:5349" },
-            { urls: "stun:stun1.l.google.com:3478" },
-            { urls: "stun:stun1.l.google.com:5349" },
-            { urls: "stun:stun2.l.google.com:19302" },
-            { urls: "stun:stun2.l.google.com:5349" },
-            { urls: "stun:stun3.l.google.com:3478" },
-            { urls: "stun:stun3.l.google.com:5349" },
-            { urls: "stun:stun4.l.google.com:19302" },
-            { urls: "stun:stun4.l.google.com:5349" },
-          ],
-        };
+      :null
 
     this.peer = null;
     this.dataChannel = null;
