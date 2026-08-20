602f6a743017b9ddcadbc75dc2ebe00be4bd0faa
TalhaKhalidAI <talhakhalid2018@gmail.com>
2026-08-21 00:19:53 +0500

    remvoe extra fluff

--- SFU_Server/src/api/validators/user.validator.js
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const updateUserSchema = z.object({
-    username: z.string().min(3).optional(),
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    avatar: z.string().url().optional(),
-});
