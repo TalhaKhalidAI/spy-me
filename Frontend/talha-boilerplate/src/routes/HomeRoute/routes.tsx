af218752fff0ab65940c84bffa2a4b874da8eddb
TalhaKhalidAI <talhakhalid2018@gmail.com>
2026-08-26 18:36:19 +0500

    route update

--- Frontend/talha-boilerplate/src/routes/HomeRoute/routes.tsx
+++ Frontend/talha-boilerplate/src/routes/HomeRoute/routes.tsx
@@ -6,10 +6,10 @@ const Login = lazy(() => import("../../pages/Login/index"))
 const Signup = lazy(() => import("../../pages/Signup/index"))
 const NotFound = lazy(() => import("../../pages/NotFound/index"))
 const Prs = lazy(() => import("../ProtectedRoute/Slice"))
-const SfuTets=lazy(()=>import("../../pages/SfuTest/"))
+const SfuTets = lazy(() => import("../../pages/SfuTest/"))
 const GuestLogin = withGuest(Login)
 const GuestSignup = withGuest(Signup)
-const SfuTestPaeg=lazy(()=>import("../../pages/SfuClientTest"))
+const SfuTestPaeg = lazy(() => import("../../pages/SfuClientTest"))
 export const routeSlice = [
   {
     path: "/",
@@ -27,13 +27,13 @@ export const routeSlice = [
     path: "/talha/*",
     component: Prs,
   },
-    {
+  {
     path: "/sfu",
     component: SfuTets,
   },
-      {
-    path: "/sfu/client",
-    component:SfuTestPaeg,
+  {
+    path: "/client",
+    component: SfuTestPaeg,
   },
   {
     path: "*",
